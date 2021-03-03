7cb7ea8eec570980e0ee64efcf93b3222c3b5bbd
Amit Chaudhary <itheamc@gmail.com>
2021-03-03 17:04:23 +0545

    added  patch and delete route for the post

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { getPosts, savePost } = require('../controllers/posts_controllers')
+const { getPosts, savePost, updatePost, deletePost } = require('../controllers/posts_controllers')
 
 const router = express.Router();
 
-router.get('/', getPosts);
-router.post('/', savePost);
+
+router.get('/', getPosts);          // Get Request
+router.post('/', savePost);         // Post Request
+router.patch('/:_id', updatePost);  // Patch Request
+router.delete('/:_id', deletePost)  // Delete Request
+
 
 
 module.exports = router;
