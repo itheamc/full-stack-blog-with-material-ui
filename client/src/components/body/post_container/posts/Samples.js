2f2faaa877c9a0f6ac15d58cc5b46a7e47e16f21
Amit Chaudhary <itheamc@gmail.com>
2021-03-05 12:08:54 +0545

    Added sample posts for testing

---
+++ client/src/components/body/post_container/posts/Samples.js
@@ -0,0 +1,28 @@
+export const posts = [
+        {
+            _id: 'akshiahfbkn',
+            post_title: 'Post title -1',
+            post_body: 'This is the description of the post title -1. These are the description of the post. I want to see how it looks like if we will add more text on this.',
+            post_image: 'http://www.freedigitalphotos.net/images/img/homepage/394230.jpg'
+
+        },
+        {
+            _id: 'dfjkdsghed',
+            post_title: 'Post title -2',
+            post_body: 'This is the description of the post title -2. These are the description of the post. I want to see how it looks like if we will add more text on this.',
+            post_image: 'https://images.indianexpress.com/2020/01/lohri.jpg'
+        },
+        {
+            _id: 'opujndjkatg',
+            post_title: 'Post title -3',
+            post_body: 'This is the description of the post title -3. These are the description of the post. I want to see how it looks like if we will add more text on this.',
+            post_image: 'https://www.searchenginejournal.com/wp-content/uploads/2018/10/How-to-Boost-Your-Images%E2%80%99-Visibility-on-Google-Images-760x400.png'
+        },
+        {
+            _id: 'ygbfyfhjdg',
+            post_title: 'Post title -4',
+            post_body: 'This is the description of the post title -4. These are the description of the post. I want to see how it looks like if we will add more text on this.',
+            post_image: 'https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg'
+
+        }
+    ];
