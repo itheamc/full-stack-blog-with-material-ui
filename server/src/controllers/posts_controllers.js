3ededa4fa720967212594f342367bd4f2a2377a4
Amit Chaudhary <itheamc@gmail.com>
2021-03-05 18:26:48 +0545

    modification in the response sent to the client

--- server/src/controllers/posts_controllers.js
+++ server/src/controllers/posts_controllers.js
@@ -10,7 +10,7 @@ const getPosts = async (req, res) => {
 
     try {
         const Posts = await PostModel.find();
-        res.status(200).json({result: "OK", statusCode: 200, posts: Posts});
+        res.status(200).json(Posts);
     } catch (error) {
         console.log(error);
         res.status(404).send("Something Went Wrong");
@@ -24,7 +24,7 @@ const savePost = async (req, res) => {
     const post_model = new PostModel(req.body);
     try {
         const post = await post_model.save();
-        res.status(201).json({result: "Created", statusCode: 201, post: post});
+        res.status(201).json(post);
         console.log(post);
     } catch (error) {
         console.log(error);
@@ -39,7 +39,7 @@ const updatePost = async (req, res) => {
     try {
         const updates = await req.body;
         const updatedPost = await PostModel.findByIdAndUpdate(req.params._id, updates);
-        res.status(202).json({result: "Updated", statusCode: 202, update_info: updatedPost});
+        res.status(202).json(updatedPost);
         console.log(updatedPost);
         
     } catch (error) {
@@ -55,7 +55,7 @@ Delete Request to delete the post
 const deletePost = async (req, res) => {
     try {
         const deletedPost = await PostModel.findByIdAndDelete(req.params._id);
-        res.status(200).json({result: "Deleted", statusCode: 200, delete_info: deletedPost});
+        res.status(200).json(deletedPost);
         console.log(deletedPost);
         
     } catch (error) {
