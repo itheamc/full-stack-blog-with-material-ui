466319100eec0d3b11517edac26f9d9f958d1175
Amit Chaudhary <itheamc@gmail.com>
2021-03-05 09:35:25 +0545

    initial commit

--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -1,47 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import BigScreenNavBar from "../header/navbar/BigScreenNavBar";
 import SmallScreenNavBar from "../header/navbar/SmallScreenNavBar";
 import BlogPagination from '../body/post_container/pagination/BlogPagination'
+import { useStyles } from './Themes'
+import { widths } from './Widths'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1
-  },
-  main_container: {
-    marginTop: 0,
-  },
-  paper: {
-    // padding: theme.spacing(2),
-    // textAlign: "center",
-    // color: theme.palette.text.secondary
-  },
-  header: {
-    backgroundColor: "red"
-  },
-  postContainer: {
-    // backgroundColor: "yellow"
-  },
-  sidebar: {
-    backgroundColor: "blue"
-  },
-  footer: {
-    backgroundColor: "orange"
-  }
-}));
 
-// Setting the max width for SmallScreen
-const maxWidth = 1200;
-// Minimum width for sidebar to be visible
-const minWidth = 1023;
+
 
 const Layout = () => {
+
   const classes = useStyles();
   const [width, setwidth] = useState(window.innerWidth);
 
-
+  // UseEffect
   useEffect(() => {
 
        // listening windows resize event
@@ -64,13 +38,13 @@ const Layout = () => {
       <Grid container spacing={1}>
         <Grid item xs={12}>
           <Paper className={`${classes.paper} ${classes.header}`}>
-              {width > maxWidth ? <BigScreenNavBar /> : <SmallScreenNavBar />}
+              {width > widths.lg ? <BigScreenNavBar /> : <SmallScreenNavBar />}
           </Paper>
         </Grid>
-        <Grid item xs={width > minWidth ? 8 : 12}>
+        <Grid item xs={width > widths.md ? 8 : 12}>
             <BlogPagination />
         </Grid>
-        <Grid item xs={width > minWidth ? 4 : false}>
+        <Grid item xs={width > widths.md ? 4 : false}>
           <Paper className={`${classes.paper} ${classes.sidebar}`}>
               
           </Paper>
---
+++ client/src/components/layout/Widths.js
@@ -0,0 +1,8 @@
+// Creating the width object for the responsive website
+export const widths = {
+  xs: 0,
+  sm: 600, 
+  md: 1024, 
+  lg: 1280, 
+  xl: 1920
+};
